db44e57ecb27211c98d09891f85f1b133f0d43ed
Abhi Sharma <me.avish143@gmail.com>
2024-12-09 00:05:25 +0530

    cors

--- backend/utils/corsOption.js
+++ backend/utils/corsOption.js
@@ -1,3 +1,4 @@
+
 const allowedOrigins = [
     'https://sasta-uber.vercel.app',
     'https://uber.abhi.works'
@@ -6,21 +7,29 @@ const allowedOrigins = [
 const corsOptions = {
     origin: function (origin, callback) {
         // Allow requests with no origin (mobile apps, curl, etc)
-        if (!origin || allowedOrigins.includes(origin)) {
+        if (!origin) {
+            callback(null, true);
+            return;
+        }
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
-            console.log("Error hogaya ");
-            
             console.error(`CORS Error: Origin ${origin} not allowed`);
             callback(new Error('Not allowed by CORS'));
         }
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'Origin'
+    ],
     exposedHeaders: ['Set-Cookie'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
     preflightContinue: false,
     optionsSuccessStatus: 204
 };
-
-export default corsOptions;
+module.exports =  corsOptions;
